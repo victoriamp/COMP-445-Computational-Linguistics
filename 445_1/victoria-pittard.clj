29945a41ca14930b3aa1d52883a3b6e7344ccf1e
Victoria Pittard <38929314+victoriamp@users.noreply.github.com>
2019-10-08 08:45:29 -0400

    completed assignment 1

---
+++ 445_1/victoria-pittard.clj
@@ -0,0 +1,72 @@
+(ns victoria-pittard)
+
+;;1
+(defn abs [x] (Math/sqrt (* x x)))
+
+;;2
+(defn take-square [x] (* x x))
+(defn sum-of-squares [x y] (+ (take-square x) (take-square y)))
+
+;;3
+(def exp-13-1 (+ 12 1))
+(def exp-13-2 (+ 10 3))
+(def exp-13-3 (+ 9 4))
+(def exp-13-4 (+ 8 5))
+
+;;4
+(defn third [x] (first (rest (rest x))))
+
+;;5
+(defn compose [f g] (fn [& n] (f (apply g n))))
+
+;;6
+(defn first-two [x] (list (first x) (first (rest x))))
+
+;;7
+(defn remove-second [x] (cons (first x) (rest (rest x))))
+
+;;8
+(def add-to-end (fn [l x]
+                    (if (empty? l)
+                      (list x)
+                      (cons (first l) (add-to-end (rest l) x)))))
+
+;;9
+(defn reverse [l]
+      (if (empty? l)
+        '()
+        (add-to-end (reverse (rest l)) (first l))))
+
+;;10
+(def count-to-1 (fn [n]
+                    (if (= 1 n)
+                      (list n)
+                      (cons n (count-to-1 (- n 1))))))
+
+;;11
+(defn count-to-n [n] (reverse (count-to-1 n)))
+
+;;12
+(def get-max (fn [l]
+                 (if (empty? l)
+                   0
+                   (max (first l) (get-max (rest l))))))
+
+;;13
+(def greater-than-five? (fn [l]
+                            (map (fn [x] (> x 5)) l)))
+
+;;14
+(defn concat-three [x y z] (flatten (cons x (cons y z))))
+
+;;15
+(def sequence-to-power (fn [x n]
+                           (if ( = n 0)
+                             '()
+                             (concat x (sequence-to-power x (- n 1))))))
+
+;;16
+(def in-L*? (fn [x]
+                (if (empty? x)
+                  true
+                  (if (= 'a (first x)) (in-L*? (rest x)) false))))
